8a3384cf94b6e766a03292bc73e69135baa35d31
Dakota Shively <122128148+DakotaShively@users.noreply.github.com>
2023-10-21 16:13:29 -0400

    Thunder Client and updated TweetRoutes 2:00

--- SOTDBackend/src/routes/tweetRoutes.ts
+++ SOTDBackend/src/routes/tweetRoutes.ts
@@ -1,26 +1,42 @@
 import { Router } from "express";
+import { PrismaClient } from "@prisma/client";
 
 const router = Router();
+const prisma = new PrismaClient();
 //Tweet CRUD
 
 //Create Tweet
-router.post("/", (req, res) => {
-	res.status(501).json({ error: "Not Implemented" });
+router.post("/", async (req, res) => {
+	const { content, image, userId } = req.body;
+	console.log(content, image, userId);
+
+	try {
+		const result = await prisma.tweet.create({
+			data: {
+				content,
+				image,
+				userId,
+			},
+		});
+
+		res.json(result);
+	} catch (e) {
+		res.status(400).json({ error: "username should be unique" });
+	}
 });
 //Get list of Tweet
-router.get("/", (req, res) => {
-	res.status(501).json({ error: "Not Implemented" });
+router.get("/", async (req, res) => {
+	const allTweets = await prisma.tweet.findMany();
+	res.json(allTweets);
 });
 
 //Get one tweet
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
 	const { id } = req.params;
-	res.status(501).json({ error: "Not Implemented: ${id}" });
-});
-
-//Update tweet
-router.put("/:id", (req, res) => {
-	const { id } = req.params;
+	const tweet = await prisma.tweet.findUnique({ where: { id: Number(id) } });
+	if (!tweet) {
+		return res.status(404).json({ error: "Post not found" });
+	}
 	res.status(501).json({ error: "Not Implemented: ${id}" });
 });
 
